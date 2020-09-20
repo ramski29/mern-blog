a47c5cef18da8e18f6df00b697141fa0e9fa8c92
rioramski <rio.ramski@ilcs.co.id>
2020-09-20 21:47:23 +0700

    update react router

---
+++ src/pages/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Routes from "../config/Routes";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
